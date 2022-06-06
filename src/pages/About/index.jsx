9ffd64e5cab0549e16b5bad833b93bf54bba4061
edwardoou <67031155+edwardoou@users.noreply.github.com>
2022-06-05 22:37:59 -0500

    add: pages admin

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,10 +1,13 @@
+import { useParams } from "react-router-dom";
+
 const About = () => {
+  const { slug } = useParams();
   return (
     <div>
-      <h1>About</h1>
-      <p>This is the about page</p>
+      <h1>About edit for me</h1>
+      <p>This is the about page {slug}</p>
     </div>
   );
-}
+};
 
 export default About;
